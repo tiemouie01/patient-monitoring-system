8bbf6554d74480523c59c161d76bea0384518371
Timothy Miamba <timal0361@gmail.com>
2024-04-24 12:14:18 +0200

    Add ui module

---
+++ src/ui.js
@@ -0,0 +1,33 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+const renderPatientInfo = (patientData) => {
+  const container = document.getElementById("patient-info");
+  container.innerHTML = `
+    <h2>Patient: ${patientData.demographics.name || "N/A" }</h2>
+    <p>Age: ${patientData.demographics.age || "N/A"}</p>
+    <p>Gender: ${patientData.demographics.gender || "N/A"}</p>
+    `;
+};
+
+const renderTimestampHistory = (timestampData) => {
+  const container = document.getElementById("timestamp-history");
+  container.innerHTML = "";
+  for (const timestampId in timestampData) {
+    const timestamp = timestampData[timestampId]; // Access data using key
+
+    const timestampElement = document.createElement('div');
+    timestampElement.classList.add('timestamp');
+    timestampElement.innerHTML = `
+      <p>Time: ${new Date(timestampId).toLocaleString()}</p>
+      <p>Heart Rate: ${timestamp.heartRate} BPM</p>
+      <p>Temperature: ${timestamp.temperature}°C</p>
+      <p>Ward: <input type="text" value="${timestamp.ward || ''}" data-timestamp-id="${timestampId}"></p>
+      <p>Alert: ${timestamp.alert ? 'Yes' : 'No'}</p>
+      <textarea data-timestamp-id="${timestampId}" placeholder="Notes">${timestamp.notes || ''}</textarea>
+      <button data-timestamp-id="${timestampId}">Save</button>
+    `;
+    container.appendChild(timestampElement);
+  };
+};
+
+export { renderPatientInfo, renderTimestampHistory };
